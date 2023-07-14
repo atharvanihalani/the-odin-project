91b617a4c3ee459d341c07a9d9fafdc4ebc060b3
atharva <113245246+atharvanihalani@users.noreply.github.com>
2023-07-14 16:12:31 -0400

    created UI for rps
    added buttons for user input; displays the score & win/lose msgs on the screen

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,27 +1,22 @@
+let rounds = 0;
+const score = [0, 0];
+const scoreDOM = document.querySelector(".score");
+const messageDOM = document.querySelector(".message");
+
 function getComputerChoice() {
     switch(Math.floor(Math.random()*3)) {
         case 0:
             return "rock";
-            break;
         case 1:
             return "paper";
-            break;
         case 2:
             return "scissors";
-            break;
         default:
-            console.error("well, shit")
+            console.error("well, shit");
     }
 }
 
-function playRound(playerChoice, computerChoice) {    
-    if (playerChoice == "gun") {
-        return "gun";
-    }
-    if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
-        return "sus";
-    }
-
+function playRound(playerChoice, computerChoice) {
     playerWins = (playerChoice == "rock" && computerChoice == "scissors") || 
                 (playerChoice == "scissors" && computerChoice == "paper") ||
                 (playerChoice == "paper" && computerChoice == "rock")
@@ -41,41 +36,52 @@ function playRound(playerChoice, computerChoice) {
     console.error("well, shit; smtn's goofy");
 }
 
-function game() {
-    const score = [0, 0];
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Play your move");
-        playerChoice = playerChoice.toLowerCase();
-        let computerChoice = getComputerChoice();
-
-        result = playRound(playerChoice, computerChoice);
-        switch (result) {
-            case 0:
-                score[0]++;
-                console.log(`${playerChoice} beats ${computerChoice}. Score: ${score[0]}:${score[1]}`);
-                break;
-            case 1:
-                score[1]++;
-                console.log(`${playerChoice} gets beaten by ${computerChoice}. Score: ${score[0]}:${score[1]}`);
-                break;
-            case 2:ga
-                console.log(`${playerChoice} and ${computerChoice} are samesies`)
-                break;
-            case "gun":
-                console.log("nice lmao");
-                i--;
-                break;
-            case "sus":
-                console.warn("hey, no cheating!");
-                i--;
-                break;
-        }
+function onClick(playerChoice = "rock") {
+    if (rounds == 5) {
+        return;
     }
-    if (score[0] > score[1]) {
-        console.log("You win!!");
-    } else if (score [0] < score[1]) {
-        console.log("You lose!");
-    } else {
-        console.log("It's a tie!");
+
+    rounds++;
+    let computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+    
+    switch (result) {
+        case 0:
+            score[0]++;
+            messageDOM.textContent = `${playerChoice} beats ${computerChoice}!!`;
+            break;
+        case 1:
+            score[1]++;
+            messageDOM.textContent = `${playerChoice} gets beaten by ${computerChoice} :(`;
+            break;
+        case 2:
+            messageDOM.textContent = `${playerChoice} and ${computerChoice} are samesies`;
+            break;
+    }
+    scoreDOM.textContent = `Score - ${score[0]} : ${score[1]}`;
+
+    if (rounds == 5) {
+        endGame();
     }
 }
+
+function endGame() {
+    const endMsg = document.createElement("h2");
+    if (score[0] > score[1]) {
+        endMsg.textContent = `GGs, u win!!`;
+    } else if (score [0] < score[1]) {
+        endMsg.textContent = `LAWL get rekt`;
+    } else {
+        endMsg.textContent = `hmm, it's a tie. play again?`;
+    }
+    scoreDOM.parentNode.appendChild(endMsg);
+
+    // TODO play again button
+}
+
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener('click', () => onClick(rockBtn.id));
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener('click', () => onClick(paperBtn.id));
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener('click', () => onClick(scissorsBtn.id));
