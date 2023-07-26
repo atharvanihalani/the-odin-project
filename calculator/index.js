fe94c33924078b4f75700d18d50628da820f3f6a
atharva <113245246+atharvanihalani@users.noreply.github.com>
2023-07-26 18:54:03 -0400

    full functionality
    need to add css

--- calculator/index.js
+++ calculator/index.js
@@ -1,3 +1,80 @@
+const display = document.getElementById('display');
+let strA = '';
+let strB = '';
+let operator;
+
+function calculate() {
+    if (strB == '') {
+        return;
+    }
+
+    let numA = +strA;
+    let numB = +strB;
+    switch (operator) {
+        case '+':
+            display.textContent = add(numA, numB);
+            break;
+        case '-':
+            display.textContent = subtract(numA, numB); 
+            break;
+        case '*':
+            display.textContent = multiply(numA, numB);
+            break;
+        case '/':
+            display.textContent = divide(numA, numB);
+            break;
+        case '%':
+            display.textContent = modulo(numA, numB);
+            break;
+        default:
+            console.error("goofy operation");
+    }
+
+    strA = display.textContent;
+    strB = '';
+    operator = null;
+}
+
+function appendNum(n) {
+    if (operator == null) {
+        strA += n;
+        display.textContent = strA;
+    } else {
+        strB += n;
+        display.textContent = strB;
+    }
+}
+
+function appendOper(o) {
+    calculate();
+    operator = o;
+}
+
+
+
+function clearDisplay() {
+    display.textContent = '';
+
+    strA = '';
+    strB = '';
+    operator = null;
+}
+
+function backspace() {
+    if (operator != null && strB == '') {
+        operator == null;
+        // deselect operator visually
+    } else if (operator != null) {
+        strB = strB.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+    } else {
+        strA = strA.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+    }
+}
+
+
+
 function add(a, b) {
     return a + b;
 }
@@ -12,27 +89,12 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b == 0) {
-        // TODO PRINT SNARKY MSG
+        console.error("ha ha, funny guy");
+        return;
     }
     return a/b;
 }
 
-
-let numA;
-let numB;
-let operator;
-
-function operator(a, oper, b) {
-    switch (oper) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
-        default:
-            return "goofy operation";
-    }
+function modulo(a, b) {
+    return ((a % b) + b) % b;
 }
