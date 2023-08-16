9e4ab7e69f71b6556f6f56c94da9264e0eb8807e
atharva <113245246+atharvanihalani@users.noreply.github.com>
2023-08-16 17:19:02 +0530

    sign-up form UI changes

--- sign-up-form/index.js
+++ sign-up-form/index.js
@@ -3,22 +3,25 @@ const emailField = document.getElementById("email");
 const passwordField = document.getElementById("password");
 const passwordCheckField = document.getElementById("password-check");
 const favAlbumField = document.getElementById("fav-album");
+const submitButton = document.querySelector('[type="submit"]');
 
-// add event listeners for all the below
-nameField.addEventListener("keydown", e => checkName());
-emailField.addEventListener("keydown", e => checkEmail());
-passwordField.addEventListener("keydown", e => checkPassword());
-passwordCheckField.addEventListener("keydown", e => checkPassword());
+nameField.addEventListener("change", e => checkName());
+emailField.addEventListener("change", e => checkEmail());
+passwordField.addEventListener("change", e => checkPassword());
+passwordCheckField.addEventListener("change", e => checkPassword());
+favAlbumField.addEventListener("change", e => checkFavAlbum());
 
 const nameRegex = /[a-z]+(\s[a-z]+)?/;
 let checkName = function() {
     if (nameRegex.test(nameField.value.toLowerCase())) {
-        console.log("heck m 123")
         nameField.classList.add("is-valid");
     } else {
-        console.log("jfdklsajl")
         nameField.classList.remove("is-valid");
+        console.log("p1 chekc name");
+        nameField.style.setProperty('--name-error-msg', "Enter your name");
+        console.log("name chekc p2");
     }
+    enableSubmit();
 }
 
 const emailRegex = /[a-z0-9_.\-]+@[a-z1-9\-]+\.[a-z1-9\-]{2,}/
@@ -28,6 +31,7 @@ let checkEmail = function() {
     } else {
         emailField.classList.remove("is-valid");
     }
+    enableSubmit();
 }
 
 const passwordRegex = /^(?=.*[^a-zA-Z0-9\s])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).+$/
@@ -39,5 +43,27 @@ let checkPassword = function() {
         } else {
             passwordField.classList.remove("is-valid");
             passwordCheckField.classList.remove("is-valid");    
+        }
+    enableSubmit();
+}
+
+let checkFavAlbum = function() {
+    if (favAlbumField.value === "") {
+        favAlbumField.classList.remove("is-valid");
+    } else {
+        favAlbumField.classList.add("is-valid");
+    }
+    enableSubmit();
+}
+
+let enableSubmit = function() {
+    if (nameField.classList.contains("is-valid") &&
+        emailField.classList.contains("is-valid") &&
+        passwordField.classList.contains("is-valid") &&
+        favAlbumField.classList.contains("is-valid")) {
+            submitButton.disabled = false;
+        } else {
+            console.log("disabled");
+            submitButton.classList.disabled = true;
         }
 }
